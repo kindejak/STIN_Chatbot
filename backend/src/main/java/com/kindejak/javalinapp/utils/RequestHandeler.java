d93dbc842460a80d3187d52bc02d00d4da5b48fa
xerox99 <jakub.kindermann@tul.cz>
2022-05-03 19:24:09 +0200

    removed unnesesary classes

--- backend/src/main/java/com/kindejak/javalinapp/utils/RequestHandeler.java
+++ backend/src/main/java/com/kindejak/javalinapp/utils/RequestHandeler.java
@@ -6,9 +6,9 @@ import io.javalin.http.Handler;
 
 public class RequestHandeler {
     public static Handler fetchResponse = ctx -> {
-        ResponseFactory responseFactory  = new ResponseFactory("en");
         ObjectMapper objectMapper = new ObjectMapper();
         BasicRequest request = objectMapper.readValue(ctx.body(),BasicRequest.class);
+        ResponseFactory responseFactory  = new ResponseFactory(request.getLanguage());
         Object response = responseFactory.getResponse(request);
         ctx.json(response);
     };
